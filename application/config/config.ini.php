a89d6fff0c67cf72abb35e99301d2a83f766a918
unknown <prince2510raj@gmail.com>
2023-01-10 15:43:15 +0530

    first ci

---
+++ application/config/config.ini.php
@@ -0,0 +1,8 @@
+<?php
+$config_base_url            =    "http://localhost/renium/"; 
+$config_project_full_name   =    "";
+$config_project_nick_name   =    "";
+$config_host_name           =    "";
+$config_db_name             =    "";
+$config_user_name           =    "";
+$config_db_password         =    "";
